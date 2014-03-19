431a13ebd81a89d00add2dd95c86ac9e5d672181
Max Korenkov <mkorenkov@gmail.com>
2014-03-19 10:54:16 +0200

    requirements

--- recipes/backup.rb
+++ recipes/backup.rb
@@ -1,10 +1,13 @@
 include_recipe 'percona::package_repo'
 
 case node['platform_family']
-when 'debian'
-  package 'xtrabackup' do
-    options '--force-yes'
-  end
+  when 'debian'
+    package 'libdbd-mysql-perl'
+    package 'libaio1'
+    package 'percona-xtrabackup'
+    #package 'xtrabackup' do
+    #  options '--force-yes'
+    #end
 when 'rhel'
   package 'percona-xtrabackup'
 end
